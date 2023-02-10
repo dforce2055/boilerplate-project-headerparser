e5c290b8ff5a7291402ccf25b3a83fb79d9d19b5
Diego Pérez <dperez2055@gmail.com>
2023-02-10 16:35:39 +0000

    adding /whoami enpoint

--- index.js
+++ index.js
@@ -15,16 +15,29 @@ app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 2
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
+app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', function(req, res) {
+  const { headers } = req
+  const ipaddress = headers['x-forwarded-for'] || ':::0'
+  const language = headers['accept-language'] || ':::0'
+  const software = headers['user-agent'] || ':::0'
+
+  res.json({
+    ipaddress,
+    language,
+    software
+  });
+});
+
 // listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, function () {
+var listener = app.listen(process.env.PORT || 3000, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
